b1cdfd2dc25f86ce2831b7c5c0fb17576ed89a2d
Antonio Oberholzer <obervall@hotmail.com>
2023-05-21 19:27:21 +0200

    Creación de nuestra primera app

---
+++ app/src/main/java/com/obervall/hellokotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.obervall.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var message: String=findViewById<TextView>(R.id.textView).text.toString()
+        Log.i("message textview", message)
+    }
+}
